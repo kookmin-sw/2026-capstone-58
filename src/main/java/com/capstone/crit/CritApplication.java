974ccaacd1953858bb0c0830dcad2718f10d141b
joonhyun2 <joony527@naver.com>
2026-03-16 16:38:06 +0900

    Initial commit - Spring Boot project

---
+++ src/main/java/com/capstone/crit/CritApplication.java
@@ -0,0 +1,13 @@
+package com.capstone.crit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CritApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CritApplication.class, args);
+	}
+
+}
